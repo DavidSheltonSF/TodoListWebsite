729e8292df1f5bbf568d0ab479c07ee8a4c19df0
DavidSheltonSF <david.faria012@gmail.com>
2026-06-28 19:38:30 -0300

    feat(home): integrate TaskStats component into the homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,6 +6,11 @@ export default function Home() {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16">
      <header className="flex flex-col">
        <h1 className="text-5xl">Todo-do</h1>
+       <TaskStats>
+        <TaskStats.Item label="Todos" value={0}/>
+        <TaskStats.Item label="Done" value={0} highlight/>
+        <TaskStats.Item label="Remaining" value={0}/>
+       </TaskStats>
      </header>
       </main>
     </div>
