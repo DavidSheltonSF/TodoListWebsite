27944cffd162d537affd181545a6ec619cd9f9b4
DavidSheltonSF <david.faria012@gmail.com>
2026-06-27 22:43:45 -0300

    fix(ITodoRepository): update parameter names for Add and Update methods for clarity

--- backend/Repositories/ITodoRepository.cs
+++ backend/Repositories/ITodoRepository.cs
@@ -5,7 +5,7 @@ public interface ITodoRepository
 {
   List<Todo> GetAll();
   Todo? GetById(int id);
-  Todo Add(Todo data);
-  Todo? Update(Todo data);
+  Todo Add(Todo todo);
+  Todo? Update(Todo todo);
   Todo? Delete(int id);
 }
