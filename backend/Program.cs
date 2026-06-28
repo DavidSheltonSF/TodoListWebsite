c6296da96906ade30d9b92d31a0419fe1df86acb
DavidSheltonSF <david.faria012@gmail.com>
2026-06-28 01:02:41 -0300

    feat(Program): add dependency injection for TodoRepository and TodoService

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,7 +1,14 @@
+using backend.Repositories;
+using backend.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Controllers
 builder.Services.AddControllers();
+
+// Dependency injection
+builder.Services.AddScoped<ITodoRepository, TodoRepository>();
+builder.Services.AddScoped<ITodoService, TodoService>();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
