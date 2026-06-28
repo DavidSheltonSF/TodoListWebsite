587822571adc257a84b7671ad9539dbe6b46e3ed
DavidSheltonSF <david.faria012@gmail.com>
2026-06-27 22:56:59 -0300

    feat(repositories): add TodoRepository

---
+++ backend/Repositories/TodoRepository.cs
@@ -0,0 +1,47 @@
+namespace backend.Repositories;
+using backend.Models;
+
+public class TodoRepository : ITodoRepository
+{
+  private readonly List<Todo> _todos = [];
+  public IEnumerable<Todo> GetAll()
+  {
+    return _todos;
+  }
+
+  public Todo? GetById(int id)
+  {
+    return _todos.FirstOrDefault((todo) => todo.Id == id);
+  }
+
+  public Todo Add(Todo todo)
+  {
+    _todos.Add(todo);
+    return todo;
+  }
+
+  public Todo? Update(Todo todo)
+  {
+    var existingTodo = GetById(todo.Id);
+
+    if (existingTodo == null)
+    {
+      return null;
+    }
+
+
+    existingTodo.Title = todo.Title;
+    existingTodo.IsCompleted = todo.IsCompleted;
+
+    return existingTodo;
+  }
+
+  public void Delete(int id)
+  {
+
+    var todo = GetById(id);
+
+    if (todo != null)
+      _todos.Remove(todo);
+  }
+}
