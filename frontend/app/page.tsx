9d902a79ba4acff65df51eeed66098cd694b15d4
DavidSheltonSF <david.faria012@gmail.com>
2026-06-28 19:38:21 -0300

    feat(home): add a title

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import { TaskStats } from "./components/TaskStats";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16">
-  
+     <header className="flex flex-col">
+       <h1 className="text-5xl">Todo-do</h1>
+     </header>
       </main>
     </div>
   );
