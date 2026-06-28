310ca84ebcd5ad07d8acf99d0f7e1d9807f306a7
DavidSheltonSF <david.faria012@gmail.com>
2026-06-28 00:08:51 -0300

    feat(services): add TodoService

---
+++ backend/services/ITodoService.cs
@@ -0,0 +1,13 @@
+using backend.Dtos;
+using backend.Models;
+
+namespace backend.Services;
+
+public interface ITodoService
+{
+  IEnumerable<Todo> GetAll();
+  Todo? GetById(int id);
+  Todo Create(CreateTodoDto data);
+  Todo Update(int id, UpdateTodoDto data);
+  void Delete(int id);
+}
---
+++ backend/services/TodoService.cs
@@ -0,0 +1,51 @@
+using System.Reflection.Metadata.Ecma335;
+using backend.Dtos;
+using backend.Models;
+using backend.Repositories;
+
+namespace backend.Services;
+
+public class TodoService(ITodoRepository repository) : ITodoService
+{
+  private readonly ITodoRepository _repository = repository;
+
+  public IEnumerable<Todo> GetAll()
+  {
+    return _repository.GetAll();
+  }
+
+  public Todo? GetById(int id)
+  {
+    return _repository.GetById(id);
+  }
+
+  public Todo Create(CreateTodoDto data)
+  {
+    var todo = new Todo
+    {
+      Title = data.Title,
+      IsCompleted = false
+    };
+
+    return _repository.Add(todo);
+  }
+
+  public Todo Update(int id, UpdateTodoDto data)
+  {
+    var todo = GetById(id) ?? throw new KeyNotFoundException("Todo not found");
+    
+    todo.Title = data.Title ?? todo.Title;
+    todo.IsCompleted = data.IsCompleted ?? todo.IsCompleted;
+
+    _repository.Update(todo);
+    return todo;
+  }
+
+  public void Delete(int id)
+{
+  if(GetById(id) == null)
+    throw new KeyNotFoundException("Todo not found");
+
+    _repository.Delete(id);
+  }
+}
