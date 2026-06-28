9e0b406f0dceffa585e6e826097b54f20af90ec7
DavidSheltonSF <david.faria012@gmail.com>
2026-06-27 22:35:46 -0300

    fix(ITodoRepository): update Add and Update methods to use Todo type instead of DTOs for consistency

--- backend/Repositories/ITodoRepository.cs
+++ backend/Repositories/ITodoRepository.cs
@@ -1,12 +1,11 @@
 namespace backend.Repositories;
-using backend.Dtos;
 using backend.Models;
 
 interface ITodoRepository
 {
   List<Todo> GetAll();
   Todo GetById(string id);
-  Todo Add(CreateTodoDto data);
-  Todo? Update(UpdateTodoDto data);
+  Todo Add(Todo data);
+  Todo? Update(Todo data);
   Todo? Delete(string id);
 }
