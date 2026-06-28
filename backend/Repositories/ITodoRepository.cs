b6e096d0038405a617b6198d94d58e90e24d2e10
DavidSheltonSF <david.faria012@gmail.com>
2026-06-27 22:37:46 -0300

    refactor(ITodoRepository): update GetById to return a nullable Todo

--- backend/Repositories/ITodoRepository.cs
+++ backend/Repositories/ITodoRepository.cs
@@ -4,7 +4,7 @@ using backend.Models;
 public interface ITodoRepository
 {
   List<Todo> GetAll();
-  Todo GetById(int id);
+  Todo? GetById(int id);
   Todo Add(Todo data);
   Todo? Update(Todo data);
   Todo? Delete(int id);
