3a03f2a66e85db90c04f3f588222484a91e6fab1
DavidSheltonSF <david.faria012@gmail.com>
2026-06-27 23:32:37 -0300

    fix(UpdateTodoDto): make Title and IsCompleted nullable for better flexibility

--- backend/Dtos/UpdateTodoDto.cs
+++ backend/Dtos/UpdateTodoDto.cs
@@ -2,6 +2,6 @@ namespace backend.Dtos;
 
 public class UpdateTodoDto
 {
-  public string Title { get; set; } = "";
-  public bool IsCompleted { get; set; }
+  public string? Title { get; set; } = "";
+  public bool? IsCompleted { get; set; }
 }
