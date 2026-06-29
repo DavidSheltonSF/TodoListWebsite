6355b354cda8bbd9e6113afed87780f685545072
DavidSheltonSF <david.faria012@gmail.com>
2026-06-28 21:16:30 -0300

    style(Home): ensure header takes full width and adjust layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
     return (
     <div className="flex flex-col flex-1 items-center justify-center">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16">
-     <header className="flex flex-col">
+     <header className="flex flex-col w-full">
        <h1 className="text-5xl">Todo-do</h1>
        <TaskStats>
         <TaskStats.Item label="Todos" value={todoStats?.all || 0}/>
