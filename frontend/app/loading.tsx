bbde1bf3d8ee0e3e238fa4ee1ebb9c127a3c085d
DavidSheltonSF <david.faria012@gmail.com>
2026-06-28 22:42:42 -0300

    refactor(loading): remove unused Loading component

--- frontend/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-import { LoadingIcon } from "./components/icons/LoadingIcon";
-
-export default function Loading(){
-  return <div className="absolute flex flex-col items-center gap-[24px] justify-center size-full">
-   <LoadingIcon className="size-[80px] animate-spin"/>
-    <h1 className="text-4xl">Loading Server...</h1>
-  </div>
-}
