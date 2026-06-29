a76be9758fe74937afeddcf21d1a018c268bd3f7
DavidSheltonSF <david.faria012@gmail.com>
2026-06-28 23:18:55 -0300

    fix(TodoArea): manage request state and error handling in TodoArea component and show a request status bar for feedback

--- frontend/app/components/TodoArea.tsx
+++ frontend/app/components/TodoArea.tsx
@@ -3,26 +3,31 @@ import { useEffect, useState } from "react";
 import { TaskStats } from "./TaskStats";
 import { TodoStats } from "../types/TodoStats";
 import { getTodoStats } from "../services/getTodoStats";
+import { RequestState } from "../types/RequestState";
+import { RequestStatusBar } from "./RequestStatusBar";
 
 export function TodoArea() {
   const [todoStats, setTodoStats] = useState<TodoStats>({all: 0, done: 0, remaining: 0});
-  const [error, setError] = useState<Error>();
+  const [requestState, setRequestState] = useState<RequestState>({status: 'loading'});
 
   useEffect(() => {
    async function loadTodoStats(){
      try {
       const stats = await getTodoStats();
       setTodoStats(stats);
+      setRequestState({status: 'ok'})
     } catch (error: any) {
-      setError(error);
+      console.log(error)
+      setRequestState({status: 'error', message: error.message});
     }
-   }
-
+    }
    loadTodoStats();
   }, []);
 
+
+
   return <div className="w-full">
-      <header className="flex flex-col w-full">
+      <header className="flex flex-col w-full gap-">
        <h1 className="text-5xl">Todo-do</h1>
        <TaskStats>
         <TaskStats.Item label="Todos" value={todoStats?.all || 0}/>
@@ -30,6 +35,7 @@ export function TodoArea() {
         <TaskStats.Item label="Remaining" value={todoStats?.remaining || 0}/>
        </TaskStats>
        <div className="border-divider"></div>
+      <RequestStatusBar requestState={requestState}/>
      </header>
   </div>
 }
