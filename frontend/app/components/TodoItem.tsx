6806a34c8c07ce93d571fabab7c0c8cede806b10
DavidSheltonSF <david.faria012@gmail.com>
2026-06-29 12:56:24 -0300

    feat(components): add TodoItem

---
+++ frontend/app/components/TodoItem.tsx
@@ -0,0 +1,20 @@
+import { Todo } from "../types/Todo"
+import { CloseIcon } from "./icons/CloseIcon";
+
+interface Props {
+  onDelete: (id: number) => void;
+  todo: Todo;
+}
+
+export function TodoItem({todo, onDelete}: Props){
+  return <article className="flex items-center w-full p-[16px] border-gray-soft bg-color-gray-dark rounded-md">
+    <div className="flex items-center gap-[16px]">
+      <input type="checkbox" className="flex items-center justify-center appearance-none text-xl size-[24px] border-gray-soft rounded-md checked:bg-[var(--color-gray)] checked:before:content-['✓']" />
+      <span>{todo.title}</span>
+    </div>
+    <button className="ml-auto cursor-pointer hover:brightness-120"
+    onClick={() => onDelete(todo.id)}>
+      <CloseIcon className="size-[24px] stroke-[var(--color-gray)]"/>
+    </button>
+  </article>
+}
