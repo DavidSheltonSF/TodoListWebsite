60aac762fd83e3cbee20e7e773ebb6b36bfe4997
DavidSheltonSF <david.faria012@gmail.com>
2026-06-29 18:29:27 -0300

    refator(TodoArea): replace getTodoStats with loadTodos and calculate stats in the client side.

--- frontend/app/components/TodoArea.tsx
+++ frontend/app/components/TodoArea.tsx
@@ -1,38 +1,40 @@
 'use client';
 import { useEffect, useState } from "react";
 import { TaskStats } from "./TaskStats";
-import { TodoStats } from "../types/TodoStats";
-import { getTodoStats } from "../services/getTodoStats";
 import { RequestState } from "../types/RequestState";
 import { RequestStatusBar } from "./RequestStatusBar";
+import { Todo } from "../types/Todo";
+import { getTodos } from "../services/getTodos";
 
 export function TodoArea() {
-  const [todoStats, setTodoStats] = useState<TodoStats>({all: 0, done: 0, remaining: 0});
+ const [todos, setTodos] = useState<Todo[]>([])
   const [requestState, setRequestState] = useState<RequestState>({status: 'loading'});
 
   useEffect(() => {
-   async function loadTodoStats(){
+   async function loadTodos(){
      try {
-      const stats = await getTodoStats();
-      setTodoStats(stats);
+      const data = await getTodos();
+      setTodos(data);
       setRequestState({status: 'ok'})
     } catch (error: any) {
       console.log(error)
       setRequestState({status: 'error', message: error.message});
     }
-    }
-   loadTodoStats();
+  }
+   loadTodos();
   }, []);
 
+  const todoDone = todos.filter((todo) => todo.isCompleted === true);
+  const todoRemaining = todos.filter((todo) => todo.isCompleted === false);
 
 
   return <div className="w-full">
       <header className="flex flex-col w-full gap-">
        <h1 className="text-5xl">Todo-do</h1>
        <TaskStats>
-        <TaskStats.Item label="Todos" value={todoStats?.all || 0}/>
-        <TaskStats.Item label="Done" value={todoStats?.done || 0} highlight/>
-        <TaskStats.Item label="Remaining" value={todoStats?.remaining || 0}/>
+        <TaskStats.Item label="Todos" value={todos.length}/>
+        <TaskStats.Item label="Done" value={todoDone.length} highlight/>
+        <TaskStats.Item label="Remaining" value={todoRemaining.length}/>
        </TaskStats>
        <div className="border-divider"></div>
       <RequestStatusBar requestState={requestState}/>
