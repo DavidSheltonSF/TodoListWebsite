98d24760952d5ff7bf3e816d65f3b8bca0c46e6b
DavidSheltonSF <david.faria012@gmail.com>
2026-06-28 23:06:33 -0300

    fix(cors): correct typo in CORS policy origin URL

--- backend/Program.cs
+++ backend/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options =>
     {
         policy
         .WithOrigins(
-            "http//localhost:3001"
+            "http://localhost:3001"
         )
         .AllowAnyHeader()
         .AllowAnyMethod();
