61aab1fd8cd6322e85ce87626816eede0b332d69
DavidSheltonSF <david.faria012@gmail.com>
2026-06-29 21:50:37 -0300

    feat(TodoItem, TodoList): add onToggleChecked prop for checkbox state management

--- frontend/app/components/TodoList.tsx
+++ frontend/app/components/TodoList.tsx
@@ -4,11 +4,12 @@ import { TodoItem } from "./TodoItem";
 interface Props {
   todos: Todo[];
   onDelete: (id: number) => void;
+  onToggleChecked: (id: number) => void;
 }
 
-export function TodoList({todos, onDelete}: Props){
+export function TodoList({todos, onDelete, onToggleChecked}: Props){
   const renderTodos = todos.map((todo) => {
-    return <TodoItem key={todo.id} todo={todo} onDelete={onDelete}/>
+    return <TodoItem key={todo.id} todo={todo} onDelete={onDelete} onToggleChecked={onToggleChecked}/>
   })
   return <div className="flex flex-col gap-[8px]">
     {renderTodos}
