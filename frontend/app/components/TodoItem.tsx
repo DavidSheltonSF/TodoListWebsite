f250eefc1f37134ccfb81fe0faebed23abfc12f5
DavidSheltonSF <david.faria012@gmail.com>
2026-07-01 13:05:27 -0300

    refactor(TodoItem): improve layout and add formatted date display

--- frontend/app/components/TodoItem.tsx
+++ frontend/app/components/TodoItem.tsx
@@ -1,3 +1,4 @@
+import { formatDateString } from "@/lib/formatDateString";
 import { Todo } from "../types/Todo"
 import { CloseIcon } from "./icons/CloseIcon";
 
@@ -9,13 +10,18 @@ interface Props {
 
 export function TodoItem({todo, onDelete, onToggleCompletion}: Props){
   return <article className="flex items-center w-full p-[16px] border-gray-soft bg-color-gray-dark rounded-md fade-in-animation">
-    <div className="flex items-center gap-[16px]">
+    <div className="flex flex-1 items-center gap-[16px]">
       <input checked={todo.isCompleted} onChange={() => onToggleCompletion(todo.id)} type="checkbox" className="flex items-center justify-center appearance-none text-xl size-[24px] border-gray-soft rounded-md checked:bg-[var(--color-gray)] checked:before:content-['✓']" />
       <span>{todo.title}</span>
     </div>
-    <button className="ml-auto cursor-pointer hover:brightness-120"
+   <div className="flex justify-end items-center flex-1 text-muted">
+     <span>{formatDateString(todo.createdAt.toString())}</span>
+   </div>
+    <div className="flex justify-end items-center ml-[8px]">
+      <button className="cursor-pointer hover:brightness-120"
     onClick={() => onDelete(todo.id)}>
       <CloseIcon className="size-[24px] stroke-[var(--color-gray)]"/>
     </button>
+    </div>
   </article>
 }
