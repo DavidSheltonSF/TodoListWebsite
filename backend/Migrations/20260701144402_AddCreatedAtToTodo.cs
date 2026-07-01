d0b0de01d717cbda79a82776528acda007ceab0f
DavidSheltonSF <david.faria012@gmail.com>
2026-07-01 12:18:34 -0300

    feat(Todo): add CreatedAt column to Todos table and update model snapshot

---
+++ backend/Migrations/20260701144402_AddCreatedAtToTodo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtToTodo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "CreatedAt",
+                table: "Todos",
+                type: "date",
+                nullable: false,
+                defaultValue: DateOnly.FromDateTime(DateTime.Now));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Todos");
+        }
+    }
+}
