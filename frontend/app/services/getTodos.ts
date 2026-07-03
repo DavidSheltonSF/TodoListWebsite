aea75f6e990c113cda1d0beeac9e446d54c4158e
DavidSheltonSF <david.faria012@gmail.com>
2026-07-03 20:20:57 -0300

    feat(frontend/services): implement pagination for getTodos

--- frontend/app/services/getTodos.ts
+++ frontend/app/services/getTodos.ts
@@ -1,8 +1,16 @@
 import { API_URL } from "@/config/ApiUrl";
 import { Todo } from "../types/Todo";
 
-export async function getTodos(): Promise<Todo[]>{
-  const response = await fetch(`${API_URL}/todos`);
+export async function getTodos(page: number, pageSize: number): Promise<Todo[]>{
+
+  const queryParams =  new URLSearchParams({
+    page: String(page) ?? '',
+    pageSice: String(pageSize ) || ''
+  });
+  
+  queryParams.set("page", String(page));
+
+  const response = await fetch(`${API_URL}/todos?${queryParams.toString()}`);
   const json = await response.json();
   return json;
 }
