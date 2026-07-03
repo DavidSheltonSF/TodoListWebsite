d1a00f886e5629564367842c5ce6972b4a521a10
DavidSheltonSF <david.faria012@gmail.com>
2026-07-03 13:23:31 -0300

    style(home): adjust padding and width for better responsiveness

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
 
   try {
     return (
-    <div className="flex flex-col flex-1 items-center justify-center text-white">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16">
+    <div className="flex flex-col flex-1 items-center justify-center text-white ">
+      <main className="flex flex-1 w-full md:max-w-2xl flex-col items-center justify-between py-[56px] px-[24px]">
         <TodoArea/>
       </main>
     </div>
