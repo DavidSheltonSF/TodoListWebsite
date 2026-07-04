0edeacecd2ab8a427960a3d7746f1f5bcb781285
DavidSheltonSF <david.faria012@gmail.com>
2026-07-04 18:12:00 -0300

    refactor(getTodos): update to return Page<Todo>

--- frontend/app/services/getTodos.ts
+++ frontend/app/services/getTodos.ts
@@ -1,8 +1,9 @@
 import { API_URL } from "@/config/ApiUrl";
 import { Todo } from "../types/Todo";
 import { TodoFilterValue } from "../types/TodoFilterValue";
+import { Page } from "../types/Page";
 
-export async function getTodos(page: number, pageSize: number, todoFilter: TodoFilterValue): Promise<Todo[]>{
+export async function getTodos(page: number, pageSize: number, todoFilter: TodoFilterValue): Promise<Page<Todo>>{
 
   const queryParams =  new URLSearchParams({
     page: String(page) ?? '',
