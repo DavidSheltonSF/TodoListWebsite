bd569fee51b7e0a8de9fadf9e249cbc41e214398
DavidSheltonSF <david.faria012@gmail.com>
2026-07-05 18:29:55 -0300

    fix(Page): correct spelling of 'TotalPages' property

--- backend/Types/Page.cs
+++ backend/Types/Page.cs
@@ -1,2 +1,2 @@
 namespace backend.Types;
-public record Page<T>(IEnumerable<T> Items, int CurrentPage, int TodalPages, int NextPage);
+public record Page<T>(IEnumerable<T> Items, int CurrentPage, int TotalPages, int NextPage);
