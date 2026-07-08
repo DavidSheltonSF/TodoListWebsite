2f43930c07a9c97e77dc2366ac349e6f41f83bef
DavidSheltonSF <david.faria012@gmail.com>
2026-07-08 12:41:43 -0300

    refactor(TodoItem, TodoItemSkeleton): improve props handling and rendering logic

--- frontend/app/components/TodoItem.tsx
+++ frontend/app/components/TodoItem.tsx
@@ -1,7 +1,7 @@
-import { formatDateString } from "@/lib/formatDateString";
-import { Todo } from "../types/Todo"
-import { CloseIcon } from "./icons/CloseIcon";
-import { TodoItemSkeleton } from "./TofoItemSkeleton";
+import { formatDateString } from '@/lib/formatDateString';
+import { Todo } from '../types/Todo';
+import { CloseIcon } from './icons/CloseIcon';
+import { TodoItemSkeleton } from './TofoItemSkeleton';
 
 interface Props {
   onDelete: (id: number) => void;
@@ -9,25 +9,33 @@ interface Props {
   todo: Todo;
 }
 
-export function TodoItem({todo, onDelete, onToggleCompletion}: Props){
-
-  if(todo.status === 'pending'){
-    return  <TodoItemSkeleton todo={todo}/>
+export function TodoItem({ todo, onDelete, onToggleCompletion }: Props) {
+  if (todo.status === 'pending') {
+    return <TodoItemSkeleton todoTitle={todo.title} />;
   }
 
-  return <article className="flex items-center w-full p-[16px] border-gray-soft bg-color-gray-dark rounded-md fade-in-animation">
-    <div className="flex flex-1 items-center gap-[16px]">
-      <input checked={todo.isCompleted} onChange={() => onToggleCompletion(todo.id)} type="checkbox" className="flex items-center justify-center appearance-none text-xl size-[24px] border-gray-soft rounded-md checked:bg-[var(--color-gray)] checked:before:content-['✓']" />
-      <span>{todo.title}</span>
-    </div>
-   <div className="flex justify-end items-center flex-1 text-muted">
-     <span>{formatDateString(todo.createdAt.toString())}</span>
-   </div>
-    <div className="flex justify-end items-center ml-[8px]">
-      <button className="cursor-pointer hover:brightness-120"
-    onClick={() => onDelete(todo.id)}>
-      <CloseIcon className="size-[24px] stroke-[var(--color-gray)]"/>
-    </button>
-    </div>
-  </article>
+  return (
+    <article className="flex items-center w-full p-[16px] border-gray-soft bg-color-gray-dark rounded-md fade-in-animation">
+      <div className="flex flex-1 items-center gap-[16px]">
+        <input
+          checked={todo.isCompleted}
+          onChange={() => onToggleCompletion(todo.id)}
+          type="checkbox"
+          className="flex items-center justify-center appearance-none text-xl size-[24px] border-gray-soft rounded-md checked:bg-[var(--color-gray)] checked:before:content-['✓']"
+        />
+        <span>{todo.title}</span>
+      </div>
+      <div className="flex justify-end items-center flex-1 text-muted">
+        <span>{formatDateString(todo.createdAt.toString())}</span>
+      </div>
+      <div className="flex justify-end items-center ml-[8px]">
+        <button
+          className="cursor-pointer hover:brightness-120"
+          onClick={() => onDelete(todo.id)}
+        >
+          <CloseIcon className="size-[24px] stroke-[var(--color-gray)]" />
+        </button>
+      </div>
+    </article>
+  );
 }
--- frontend/app/components/TofoItemSkeleton.tsx
+++ frontend/app/components/TofoItemSkeleton.tsx
@@ -1,20 +1,15 @@
-import { formatDateString } from '@/lib/formatDateString';
-import { Todo } from '../types/Todo';
 import { LoadingIcon } from './icons/LoadingIcon';
 
 interface Props {
-  todo: Todo;
+  todoTitle: string;
 }
 
-export function TodoItemSkeleton({ todo }: Props) {
+export function TodoItemSkeleton({ todoTitle }: Props) {
   return (
     <article className="flex items-center w-full p-[16px] border-gray-soft bg-color-gray-dark rounded-md fade-in-animation">
       <div className="flex flex-1 items-center gap-[16px]">
         <LoadingIcon className="size-[24px] animate-spin" />
-        <span>{todo.title}</span>
-      </div>
-      <div className="flex justify-end items-center flex-1 text-muted">
-        <span>{formatDateString(todo.createdAt.toString())}</span>
+        <span>{todoTitle}</span>
       </div>
     </article>
   );
