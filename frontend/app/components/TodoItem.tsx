93e4d8e1c4ca1c6f41025515ae2eba60a5484e80
DavidSheltonSF <david.faria012@gmail.com>
2026-07-08 12:15:22 -0300

    refactor(TodoItem): render TodoItemSkeleton if todo is pending

--- frontend/app/components/TodoItem.tsx
+++ frontend/app/components/TodoItem.tsx
@@ -1,6 +1,7 @@
 import { formatDateString } from "@/lib/formatDateString";
 import { Todo } from "../types/Todo"
 import { CloseIcon } from "./icons/CloseIcon";
+import { TodoItemSkeleton } from "./TofoItemSkeleton";
 
 interface Props {
   onDelete: (id: number) => void;
@@ -9,6 +10,11 @@ interface Props {
 }
 
 export function TodoItem({todo, onDelete, onToggleCompletion}: Props){
+
+  if(todo.status === 'pending'){
+    return  <TodoItemSkeleton todo={todo}/>
+  }
+
   return <article className="flex items-center w-full p-[16px] border-gray-soft bg-color-gray-dark rounded-md fade-in-animation">
     <div className="flex flex-1 items-center gap-[16px]">
       <input checked={todo.isCompleted} onChange={() => onToggleCompletion(todo.id)} type="checkbox" className="flex items-center justify-center appearance-none text-xl size-[24px] border-gray-soft rounded-md checked:bg-[var(--color-gray)] checked:before:content-['✓']" />
