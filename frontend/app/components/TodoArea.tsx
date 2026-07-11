6976632ef4595a70ea5ee21883d06677d05a08fa
DavidSheltonSF <david.faria012@gmail.com>
2026-07-11 20:30:30 -0300

    refactor(TodoArea, useTodos): move filtering logic to useTodos

--- frontend/app/components/TodoArea.tsx
+++ frontend/app/components/TodoArea.tsx
@@ -1,73 +1,81 @@
 'use client';
-import { useState } from "react";
-import { TaskStats } from "./TaskStats";
-import { AddIcon } from "./icons/AddIcon";
-import { TodoList } from "./TodoList";
-import { TodoFilter } from "./TodoFilter";
-import { Button } from "./Button";
-import { useTodos } from "@/hooks/useTodos";
-import { RequestStatusBar } from "./RequestStatusBar";
+import { useState } from 'react';
+import { TaskStats } from './TaskStats';
+import { AddIcon } from './icons/AddIcon';
+import { TodoList } from './TodoList';
+import { TodoFilter } from './TodoFilter';
+import { Button } from './Button';
+import { useTodos } from '@/hooks/useTodos';
+import { RequestStatusBar } from './RequestStatusBar';
 
 export function TodoArea() {
   const {
-    todos, 
-    filter, 
-    stats, 
-    nextPage, 
+    todos,
+    filter,
+    stats,
+    nextPage,
     handleCreateTodo,
     handleDelete,
     handleChangeFilter,
     handleLoadMore,
     handleToggleTodo,
-    requestState
+    requestState,
   } = useTodos();
-  const [inputText, setInputText] = useState("");
+  const [inputText, setInputText] = useState('');
 
-  const doneTodos = todos.filter((todo) => todo.isCompleted);
-  const remainingTodos = todos.filter((todo) => !todo.isCompleted);
-  const filteredTodos = filter === 'done' ? doneTodos: remainingTodos
-
-  return <div className="flex flex-col gap-[24px] w-full">
+  return (
+    <div className="flex flex-col gap-[24px] w-full">
       <header className="flex flex-col w-full gap-">
-       <h1 className="text-5xl">Todo-do</h1>
-       <TaskStats>
-        <TaskStats.Item label="Todos" value={stats?.all ?? 0}/>
-        <TaskStats.Item label="Done" value={stats?.done ?? 0} highlight/>
-        <TaskStats.Item label="Remaining" value={stats?.remaining ?? 0}/>
-       </TaskStats>
-       <div className="border-divider"></div>
-      <RequestStatusBar requestState={requestState}/>
-     </header>
-     <div className="flex flex-col gap-[24px]">
-      <TodoFilter selectedValue={filter} updateFilter={handleChangeFilter}/>
-      <form className="flex gap-[16px]" onSubmit={ async (e: any) => {
-        e.preventDefault();
-        const data = new FormData(e.currentTarget);
-        const title = data.get('todoTitle');
-        if(!title) return;
+        <h1 className="text-5xl">Todo-do</h1>
+        <TaskStats>
+          <TaskStats.Item label="Todos" value={stats?.all ?? 0} />
+          <TaskStats.Item label="Done" value={stats?.done ?? 0} highlight />
+          <TaskStats.Item label="Remaining" value={stats?.remaining ?? 0} />
+        </TaskStats>
+        <div className="border-divider"></div>
+        <RequestStatusBar requestState={requestState} />
+      </header>
+      <div className="flex flex-col gap-[24px]">
+        <TodoFilter selectedValue={filter} updateFilter={handleChangeFilter} />
+        <form
+          className="flex gap-[16px]"
+          onSubmit={async (e: any) => {
+            e.preventDefault();
+            const data = new FormData(e.currentTarget);
+            const title = data.get('todoTitle');
+            if (!title) return;
 
-        handleCreateTodo(String(title))
-        
-        setInputText('');
-        
-      }}>
-        <input 
-        value={inputText} 
-        onChange={(e) => setInputText(e.target.value)} 
-        name='todoTitle' type="text" className="bg-color-gray-dark w-full rounded-md p-[8px] border border-[var(--color-gray)] focus:border-[var(--color-green)] transition-[border] duration-300"/>
-        <Button type="submit" className="p-[8px] bg-color-green">
-          <AddIcon className="stroke-black size-[24px]"/>
-        </Button>
-      </form>
-      <TodoList 
-      todos={filter === 'all' ? todos : filteredTodos} 
-      todoFilterValue={filter}
-      onDelete={handleDelete} 
-      onToggleCompletion={handleToggleTodo}/>
-     {
-      nextPage > 0 
-      && <Button className="bg-color-green-dark text-green" onClick={handleLoadMore}>Load More</Button>
-     }
-     </div>
-  </div>
+            handleCreateTodo(String(title));
+
+            setInputText('');
+          }}
+        >
+          <input
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            name="todoTitle"
+            type="text"
+            className="bg-color-gray-dark w-full rounded-md p-[8px] border border-[var(--color-gray)] focus:border-[var(--color-green)] transition-[border] duration-300"
+          />
+          <Button type="submit" className="p-[8px] bg-color-green">
+            <AddIcon className="stroke-black size-[24px]" />
+          </Button>
+        </form>
+        <TodoList
+          todos={todos}
+          todoFilterValue={filter}
+          onDelete={handleDelete}
+          onToggleCompletion={handleToggleTodo}
+        />
+        {nextPage > 0 && (
+          <Button
+            className="bg-color-green-dark text-green"
+            onClick={handleLoadMore}
+          >
+            Load More
+          </Button>
+        )}
+      </div>
+    </div>
+  );
 }
--- frontend/hooks/useTodos.ts
+++ frontend/hooks/useTodos.ts
@@ -1,113 +1,118 @@
-import { createTodo } from "@/app/services/createTodo";
-import { deleteTodo } from "@/app/services/deleteTodo";
-import { getTodos } from "@/app/services/getTodos";
-import { getTodoStats } from "@/app/services/getTodoStats";
-import { toggleTodo } from "@/app/services/toggleTodo";
-import { RequestState } from "@/app/types/RequestState";
-import { Todo } from "@/app/types/Todo";
-import { TodoFilterValue } from "@/app/types/TodoFilterValue";
-import { TodoStats } from "@/app/types/TodoStats";
-import { formatDateString } from "@/lib/formatDateString";
-import { generateTempId } from "@/lib/generateTempId";
-import { useEffect, useState } from "react";
+import { createTodo } from '@/app/services/createTodo';
+import { deleteTodo } from '@/app/services/deleteTodo';
+import { getTodos } from '@/app/services/getTodos';
+import { getTodoStats } from '@/app/services/getTodoStats';
+import { toggleTodo } from '@/app/services/toggleTodo';
+import { RequestState } from '@/app/types/RequestState';
+import { Todo } from '@/app/types/Todo';
+import { TodoFilterValue } from '@/app/types/TodoFilterValue';
+import { TodoStats } from '@/app/types/TodoStats';
+import { formatDateString } from '@/lib/formatDateString';
+import { generateTempId } from '@/lib/generateTempId';
+import { useEffect, useState } from 'react';
 
 export function useTodos() {
   const [currentPage, setCurrentPage] = useState(1);
   const [nextPage, setNextPage] = useState(0);
-  const [todos, setTodos] = useState<Todo[]>([])
-  const [filter, setFilter] = useState<TodoFilterValue>("all");
-  const [stats, setStats] = useState<TodoStats>({all: 0, done: 0, remaining: 0})
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState<TodoFilterValue>('all');
+  const [stats, setStats] = useState<TodoStats>({
+    all: 0,
+    done: 0,
+    remaining: 0,
+  });
   const [requestState, setRequestState] = useState<RequestState | null>(null);
 
   useEffect(() => {
     async function fetchTodos() {
       try {
-        setRequestState({status: 'loading'})
-        const todosPromise = getTodos(
-          currentPage,
-          10,
-          filter
-        )
+        setRequestState({ status: 'loading' });
+        const todosPromise = getTodos(currentPage, 10, filter);
 
-        const statsPromise = getTodoStats()
+        const statsPromise = getTodoStats();
 
         const [todosResponse, statsResponse] = await Promise.all([
           todosPromise,
-          statsPromise
+          statsPromise,
         ]);
 
         const replace = currentPage === 1;
 
         setCurrentPage(todosResponse.currentPage);
         setNextPage(todosResponse.nextPage);
-        setTodos((prev) => replace ? todosResponse.items : ([...prev, ...todosResponse.items]));
+        setTodos((prev) =>
+          replace ? todosResponse.items : [...prev, ...todosResponse.items]
+        );
         setStats(statsResponse);
-        setRequestState({status: 'ok'})
-      } catch(error: any) {
-        console.log(error)
-        setRequestState({status: 'error', message: error.message})
-      } 
+        setRequestState({ status: 'ok' });
+      } catch (error: any) {
+        console.log(error);
+        setRequestState({ status: 'error', message: error.message });
+      }
     }
 
     fetchTodos();
-  } ,[currentPage, filter]);
-
+  }, [currentPage, filter]);
 
   function sortTodos() {
-    setTodos((prev) => prev.toSorted((a, b) => {
-      const aDate = new Date(a.createdAt);
-      const bDate = new Date(b.createdAt);
+    setTodos((prev) =>
+      prev.toSorted((a, b) => {
+        const aDate = new Date(a.createdAt);
+        const bDate = new Date(b.createdAt);
 
-      return bDate.getTime() - aDate.getTime();
-    }));
+        return bDate.getTime() - aDate.getTime();
+      })
+    );
   }
 
-  function handleLoadMore(){
+  function handleLoadMore() {
     setCurrentPage((prev) => prev + 1);
   }
 
-  function increaseAll(){
-  setStats((prev) => ({...prev, all: prev.all + 1}))  }
-
-  function decreaseAll() {
-    if(stats.all === 0) {
-      return;
-    }
-    setStats((prev) => ({...prev, all: prev.all - 1}));
+  function increaseAll() {
+    setStats((prev) => ({ ...prev, all: prev.all + 1 }));
   }
 
-   function inCreaseDone(){
-    setStats((prev) => ({...prev, done: prev.done + 1}))  }
-
-  function decreaseDone() {
-    if(stats.done === 0) {
+  function decreaseAll() {
+    if (stats.all === 0) {
       return;
     }
-    setStats((prev) => ({...prev, done: prev.done - 1}));
+    setStats((prev) => ({ ...prev, all: prev.all - 1 }));
+  }
+
+  function inCreaseDone() {
+    setStats((prev) => ({ ...prev, done: prev.done + 1 }));
+  }
+
+  function decreaseDone() {
+    if (stats.done === 0) {
+      return;
+    }
+    setStats((prev) => ({ ...prev, done: prev.done - 1 }));
   }
 
   function inCreaseRemaining() {
-    setStats((prev) => ({...prev, remaining: prev.remaining + 1}));
+    setStats((prev) => ({ ...prev, remaining: prev.remaining + 1 }));
   }
 
   function decreaseRemainig() {
-    if(stats.remaining === 0){
+    if (stats.remaining === 0) {
       return;
     }
-    setStats((prev) => ({...prev, remaining: prev.remaining - 1}));
+    setStats((prev) => ({ ...prev, remaining: prev.remaining - 1 }));
   }
 
   function toggleStats(wasCompleted: boolean) {
-    if(wasCompleted){
-        inCreaseRemaining();
-        decreaseDone();
-      } else {
-        inCreaseDone();
-        decreaseRemainig();
-      }
+    if (wasCompleted) {
+      inCreaseRemaining();
+      decreaseDone();
+    } else {
+      inCreaseDone();
+      decreaseRemainig();
+    }
   }
 
-  async function handleCreateTodo(title: string){
+  async function handleCreateTodo(title: string) {
     const pendingTodo: Todo = {
       id: generateTempId(),
       title,
@@ -116,84 +121,98 @@ export function useTodos() {
       status: 'pending',
     };
     try {
-      setTodos((prev) => ([pendingTodo, ...prev]));
+      setTodos((prev) => [pendingTodo, ...prev]);
       increaseAll();
       inCreaseRemaining();
-      setRequestState({status: 'loading'});
+      setRequestState({ status: 'loading' });
       const todo = await createTodo(title);
-      setTodos((prev) => prev.map((t) => t.id === pendingTodo.id ? todo : t));
+      setTodos((prev) => prev.map((t) => (t.id === pendingTodo.id ? todo : t)));
       sortTodos();
-      setRequestState({status: 'ok'});
+      setRequestState({ status: 'ok' });
     } catch (error: any) {
       setTodos((prev) => prev.filter((t) => t.id !== pendingTodo.id));
       decreaseAll();
       decreaseRemainig();
       console.log(error);
-      setRequestState({status: 'error', message: error.message});
+      setRequestState({ status: 'error', message: error.message });
     }
   }
 
-  async function handleDelete(id: number){
+  async function handleDelete(id: number) {
     const todo = todos.find((t) => t.id === id);
-    if(!todo) return;
+    if (!todo) return;
 
     try {
-      setRequestState({status: 'loading'});
+      setRequestState({ status: 'loading' });
       setTodos((prev) => prev.filter((t) => t.id !== id));
       await deleteTodo(id);
-      setRequestState(({status: 'ok'}));
+      setRequestState({ status: 'ok' });
 
-      const wasCompleted = todo.isCompleted
+      const wasCompleted = todo.isCompleted;
 
-      if(wasCompleted){
+      if (wasCompleted) {
         decreaseDone();
       } else {
-
         decreaseRemainig();
       }
 
       decreaseAll();
-
-    } catch(error: any){
-      setTodos((prev) => ([...prev, todo]))
-      setRequestState({status: 'error', message: error.message})
+    } catch (error: any) {
+      setTodos((prev) => [...prev, todo]);
+      setRequestState({ status: 'error', message: error.message });
     }
   }
 
   async function handleToggleTodo(id: number) {
     const todo = todos.find((t) => t.id === id);
-    if(!todo) return;
+    if (!todo) return;
     try {
-      setRequestState({status: 'loading'});
-      const updatedTodo = {...todo};
-      updatedTodo.isCompleted = !todo.isCompleted
-      setTodos((prev) => prev.map((t) => t.id === id ? updatedTodo : t ));
+      setRequestState({ status: 'loading' });
+      const updatedTodo = { ...todo };
+      updatedTodo.isCompleted = !todo.isCompleted;
+      setTodos((prev) => prev.map((t) => (t.id === id ? updatedTodo : t)));
 
-      toggleStats(todo.isCompleted)
+      toggleStats(todo.isCompleted);
       await toggleTodo(id);
-      setRequestState({status: 'ok'})
+      setRequestState({ status: 'ok' });
     } catch (error: any) {
-      setTodos((prev) => prev.map((t) => t.id === id ? todo : t));
-      setRequestState({status: 'error', message: error.message});
+      setTodos((prev) => prev.map((t) => (t.id === id ? todo : t)));
+      setRequestState({ status: 'error', message: error.message });
     }
   }
 
-  async function handleChangeFilter(value: TodoFilterValue){
+  async function handleChangeFilter(value: TodoFilterValue) {
     setFilter(value);
     setCurrentPage(1);
   }
 
+  function filterTodos() {
+    if (filter === 'all') {
+      return todos;
+    }
+
+    return todos.filter((todo) =>
+      filter === 'done' ? todo.isCompleted : !todo.isCompleted
+    );
+  }
+
+  const filteredTodos = filter === 'all'
+  ? todos 
+  : filter === 'done'
+    ? todos.filter((t) => t.isCompleted)
+    : todos.filter((t) => !t.isCompleted)
+
   return {
-    todos, 
-    stats, 
+    todos: filteredTodos,
+    stats,
     currentPage,
-    nextPage, 
-    filter, 
+    nextPage,
+    filter,
     requestState,
     handleCreateTodo,
     handleDelete,
     handleChangeFilter,
     handleLoadMore,
     handleToggleTodo,
-  }
+  };
 }
