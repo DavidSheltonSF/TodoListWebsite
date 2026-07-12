143956d44d0635a8526787dd5d8a22b05049659a
DavidSheltonSF <david.faria012@gmail.com>
2026-07-12 16:43:24 -0300

    style(TaskStatsItem): format code for consistency and readability

--- frontend/app/components/TaskStatsItem.tsx
+++ frontend/app/components/TaskStatsItem.tsx
@@ -4,9 +4,13 @@ interface Props {
   highlight?: boolean;
 }
 
-export function TaskStatsItem({value, label, highlight=false}: Props) {
-  return <div className="flex flex-col">
-    <span className={`text-3xl ${highlight &&  'text-[var(--color-green)]'}`}>{value}</span>
-    <span className="text-xl text-muted">{label}</span>
-  </div>
+export function TaskStatsItem({ value, label, highlight = false }: Props) {
+  return (
+    <div className="flex flex-col">
+      <span className={`text-3xl ${highlight && 'text-[var(--color-green)]'}`}>
+        {value}
+      </span>
+      <span className="text-xl text-muted">{label}</span>
+    </div>
+  );
 }
