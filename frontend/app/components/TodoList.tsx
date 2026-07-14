d7b68989d570e0271d3cac2c8a9aa995f319bc21
DavidSheltonSF <david.faria012@gmail.com>
2026-07-14 13:44:02 -0300

    chore(TodoList): enhance code formatting for better readability

--- frontend/app/components/TodoList.tsx
+++ frontend/app/components/TodoList.tsx
@@ -1,6 +1,6 @@
-import { Todo } from "../types/Todo";
-import { TodoFilterValue } from "../types/TodoFilterValue";
-import { TodoItem } from "./TodoItem";
+import { Todo } from '../types/Todo';
+import { TodoFilterValue } from '../types/TodoFilterValue';
+import { TodoItem } from './TodoItem';
 
 interface Props {
   todos: Todo[];
@@ -9,33 +9,52 @@ interface Props {
   onToggleCompletion: (id: number) => void;
 }
 
-export function TodoList({todos, todoFilterValue, onDelete, onToggleCompletion}: Props){
+export function TodoList({
+  todos,
+  todoFilterValue,
+  onDelete,
+  onToggleCompletion,
+}: Props) {
   const renderTodos = todos.map((todo) => {
-    return <TodoItem key={todo.id} todo={todo} onDelete={onDelete} onToggleCompletion={onToggleCompletion}/>
+    return (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        onDelete={onDelete}
+        onToggleCompletion={onToggleCompletion}
+      />
+    );
   });
 
   const emptyState = {
     all: {
-      sign: "○",
-      message:"Add your first task above"
+      sign: '○',
+      message: 'Add your first task above',
     },
     done: {
-      sign: "✓",
-      message: "Nothing completed yet"
+      sign: '✓',
+      message: 'Nothing completed yet',
     },
     remaining: {
-      sign: "○",
-      message: "Add your first task above"
-    }
-  }
+      sign: '○',
+      message: 'Add your first task above',
+    },
+  };
 
-  return <div className="flex flex-col gap-[8px] min-h-[200px]">
-    {
-    todos.length > 0 
-    ? renderTodos
-    : <div className="flex flex-col gap-[16px] justify-center items-center size-full">
-      <span className="text-muted text-5xl">{emptyState[todoFilterValue].sign}</span>
-      <span className="text-muted">{emptyState[todoFilterValue].message}</span>
-      </div>}
-  </div>
+  return (
+    <div className="flex flex-col gap-[8px] min-h-[200px]">
+      {todos.length > 0 ? (
+        renderTodos
+      ) : (
+        <div className="flex flex-col gap-[16px] justify-center items-center size-full">
+          <span className="text-muted text-5xl">
+            {emptyState[todoFilterValue].sign}
+          </span>
+          <span className="text-muted">
+            {emptyState[todoFilterValue].message}
+          </span>
+        </div>
+      )}
+    </div>
+  );
 }
